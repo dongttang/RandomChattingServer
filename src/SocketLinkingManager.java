2632efaa8235e1d318bf0e850b1de6819ef77aa5
DongHoon <dhdnzk0304@gmail.com>
2016-01-21 11:24:42 +0900

    생성된 채팅 방 개수 확인 및 출력로그 구현

--- src/SocketLinkingManager.java
+++ src/SocketLinkingManager.java
@@ -7,9 +7,11 @@ public class SocketLinkingManager extends Thread{
     Queue<Socket>           socketQueue;
     Socket                  socket1;
     Socket                  socket2;
+    int[] numOfLinkedClientPair;
 
-    public SocketLinkingManager(LinkedList<Socket> socketQueue) {
+    public SocketLinkingManager(LinkedList<Socket> socketQueue, int[] numOfLinkedClientPair) {
         this.socketQueue = socketQueue;
+        this.numOfLinkedClientPair = numOfLinkedClientPair;
     }
 
     @Override
@@ -21,8 +23,10 @@ public class SocketLinkingManager extends Thread{
                 if (socketQueue.size() >= 2) {
                     socket1 = socketQueue.poll();
                     socket2 = socketQueue.poll();
-                    new MessageSendingThread(socket1, socket2).start();
-                    new MessageSendingThread(socket2, socket1).start();
+                    new MessageSendingThread(socket1, socket2, numOfLinkedClientPair).start();
+                    new MessageSendingThread(socket2, socket1, numOfLinkedClientPair).start();
+                    numOfLinkedClientPair[0] ++;
+                    System.out.println( "\n현재 채팅방 수 : " + numOfLinkedClientPair[0] + "개");
                 }
             }
             socket1 = null;
